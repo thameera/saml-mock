b833a69424987e708136a9b93f933d240f2805ab
Thameera Senanayaka <me@thameera.com>
2023-09-16 10:20:53 +1000

    Theme the xml editor
    The theme was created manually, both because of some babel errors
    in official themes and also to fine-tune the colors to preference.

--- components/XMLEditor.js
+++ components/XMLEditor.js
@@ -1,7 +1,39 @@
 import { useEffect, useState } from 'react'
 import CodeMirror from '@uiw/react-codemirror'
 import { xml } from '@codemirror/lang-xml'
+import { createTheme } from '@uiw/codemirror-themes'
+import { tags as t } from '@lezer/highlight'
 import styles from './XMLEditor.module.css'
+
+const xmlTheme = createTheme({
+  theme: 'light',
+  settings: {
+    background: '#ffffff',
+    foreground: '#000000', // Regular text
+    caret: '#5d00ff',
+    selection: '#036dd626',
+    selectionMatch: '#036dd626',
+    lineHighlight: '#8a91991a',
+    gutterBackground: '#fff',
+    gutterForeground: '#8a919966',
+  },
+  styles: [
+    { tag: [t.meta, t.comment], color: '#804000' },
+    { tag: [t.keyword, t.strong], color: '#0000FF' },
+    { tag: [t.number], color: '#FF0080' },
+    { tag: [t.string], color: '#FF0000' }, // String in quotes
+    { tag: [t.attributeName], color: '#75167D' }, // Attribute name
+    { tag: [t.variableName], color: '#006600' },
+    { tag: [t.escape], color: '#33CC33' },
+    { tag: [t.tagName, t.angleBracket], color: '#507E7F' }, // Tag name, angle bracket
+    { tag: [t.heading], color: '#0C07FF' },
+    { tag: [t.quote], color: '#000000' },
+    { tag: [t.list], color: '#B90690' },
+    { tag: [t.documentMeta], color: '#888888' },
+    { tag: [t.function(t.variableName)], color: '#0000A2' },
+    { tag: [t.definition(t.typeName), t.typeName], color: '#6D79DE' },
+  ],
+})
 
 export default function XMLEditor({ xmlStr, updateXmlStr, disabled }) {
   const [mounted, setMounted] = useState(false)
@@ -28,6 +60,7 @@ export default function XMLEditor({ xmlStr, updateXmlStr, disabled }) {
           }}
           onChange={handleChange}
           extensions={[xml()]}
+          theme={xmlTheme}
         />
       )}
     </>
