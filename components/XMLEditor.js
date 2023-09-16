43c9ca8b7975abd8c068c9b0da8f368c8f985cc9
Thameera Senanayaka <me@thameera.com>
2023-09-16 10:23:01 +1000

    Clean up old CodeMirror remnants

--- components/XMLEditor.js
+++ components/XMLEditor.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import CodeMirror from '@uiw/react-codemirror'
 import { xml } from '@codemirror/lang-xml'
 import { createTheme } from '@uiw/codemirror-themes'
@@ -36,33 +35,23 @@ const xmlTheme = createTheme({
 })
 
 export default function XMLEditor({ xmlStr, updateXmlStr, disabled }) {
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    // This component's code needs a browser context and useEffect will run only in a browser
-    // https://github.com/vercel/next.js/discussions/17443#discussioncomment-87097
-    setMounted(true)
-  }, [])
-
   const handleChange = (value) => {
     updateXmlStr && updateXmlStr(value)
   }
 
   return (
     <>
-      {mounted && (
-        <CodeMirror
-          value={xmlStr}
-          className={styles.cmDiv}
-          options={{
-            mode: disabled ? 'plain' : 'xml',
-            readOnly: disabled,
-          }}
-          onChange={handleChange}
-          extensions={[xml()]}
-          theme={xmlTheme}
-        />
-      )}
+      <CodeMirror
+        value={xmlStr}
+        className={styles.cmDiv}
+        options={{
+          mode: disabled ? 'plain' : 'xml',
+          readOnly: disabled,
+        }}
+        onChange={handleChange}
+        extensions={[xml()]}
+        theme={xmlTheme}
+      />
     </>
   )
 }
