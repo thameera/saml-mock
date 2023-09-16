e2daaf967a602894f8fe9e97e808b492fb6e428f
Thameera Senanayaka <me@thameera.com>
2023-09-16 11:45:34 +1000

    Recreate MUI theme, update styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,23 @@
-import { CssBaseline, unstable_createMuiStrictModeTheme } from '@mui/material'
-//import { ThemeProvider } from '@mui/styles'
+import { CssBaseline } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 import '../styles/globals.css'
 
-// Using this (instead of createTheme) to get rid of React strict mode warnings
-// https://stackoverflow.com/a/64135466/390522
-// const theme = unstable_createMuiStrictModeTheme({
-//   palette: {
-//     primary: {
-//       main: '#076b26',
-//     },
-//   },
-// })
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#076b26',
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <CssBaseline />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
