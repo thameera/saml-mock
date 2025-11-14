67dbe70d749f1f43cf1d2523b08ada6d7c1c20e3
Adam Mcgrath <adam.mcgrath@okta.com>
2025-11-14 13:52:08 +0000

    Add option to skip embedding KeyInfo

--- pages/idp.js
+++ pages/idp.js
@@ -44,6 +44,7 @@ export default function IdP(props) {
     signResponse: false,
     sigAlgo: 'rsa-sha1',
     digestAlgo: 'sha1',
+    embedKeyInfo: true,
   })
   const [sendResponse, setSendResponse] = useState(true)
   const [sendRelayState, setSendRelayState] = useState(true)
@@ -289,6 +290,23 @@ export default function IdP(props) {
                     />
                   }
                   label="Sign Response"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={sigOpts.embedKeyInfo}
+                      onChange={(ev) =>
+                        setSigOpts({
+                          ...sigOpts,
+                          embedKeyInfo: ev.target.checked,
+                        })
+                      }
+                      disabled={!sendResponse}
+                      name="embedKeyInfo"
+                      color="primary"
+                    />
+                  }
+                  label="Embed KeyInfo"
                 />
                 <FormControl variant="standard" className={styles.select}>
                   <InputLabel id="sig-algo">Signature Algorithm</InputLabel>
